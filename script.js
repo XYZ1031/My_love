d884e6b8bcd32969f40daa3a5e17e7f4b7301621
XYZ1031 <106008184+XYZ1031@users.noreply.github.com>
2022-11-20 18:15:48 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const toggle = document.querySelector('.lit')
 
 toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html')
+    const html = document.querySelector('')//html
     if (html.classList.contains('white')) {
         html.classList.remove('white')
        
